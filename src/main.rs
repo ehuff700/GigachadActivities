829bdd3de886759602dc3f92cce3c17bd3c773bd
Evan <ehuff007@gmail.com>
2023-10-08 16:13:04 -0400

    feat: reconnection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::sync::atomic::AtomicU8;
-use std::time::Duration;
+use std::{sync::atomic::AtomicU8, time::Duration};
 
-use discord_rich_presence::activity::Activity;
-use discord_rich_presence::activity::Assets;
-use discord_rich_presence::DiscordIpc;
-use discord_rich_presence::DiscordIpcClient;
+use discord_rich_presence::{
+    activity::{Activity, Assets},
+    DiscordIpc, DiscordIpcClient,
+};
+use tracing::{error, info};
+use tracing_subscriber::filter::LevelFilter;
 
 const DETAILS: [&str; 6] = [
     "Solving world hunger",
@@ -12,21 +13,44 @@ const DETAILS: [&str; 6] = [
     "Lobbying for world peace",
     "Reversing Climate Change",
     "Eradicating Infectious Diseases",
-    "Ensuring Universal Access to Education",
+    "Fighting Crime (I'm Batman)",
 ];
 
 static INDEX: AtomicU8 = AtomicU8::new(0);
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = DiscordIpcClient::new("1160301584635404299")?;
+/// Function that will infinitely attempt to reconnect to Discord on connection failure.
+fn attempt_reconnection(client: &mut DiscordIpcClient) {
+    loop {
+        info!("Connection lost, reconnecting...");
+        if client.connect().is_ok() {
+            info!("Successfully reconnected to Discord.");
+            break;
+        } else {
+            error!("Reconnection attempt failed. Trying again in 3 seconds.");
+        }
+        std::thread::sleep(Duration::from_secs(3));
+    }
+}
 
-    client.connect()?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
+
+    let mut client = DiscordIpcClient::new("1160301584635404299")?;
 
     let activity_base = Activity::new().assets(
         Assets::new()
             .large_image("gigachad2")
             .large_text("A true gigachad"),
     );
+
+    if let Err(e) = client.connect() {
+        error!("Error connecting to Discord: {}", e);
+        attempt_reconnection(&mut client);
+    };
+
+    info!("Successfully connected to Discord.");
 
     loop {
         let clone = activity_base.clone();
@@ -37,11 +61,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             index = 0;
         }
 
-        println!("index: {}", index);
-        println!("index as usize: {}", index as usize);
+        info!("Switching activity to: \"{}\".", DETAILS[index as usize]);
 
         if let Err(why) = client.set_activity(clone.details(DETAILS[index as usize])) {
-            eprintln!("{}", why);
+            error!("ERROR: {}", why);
+            attempt_reconnection(&mut client);
         };
 
         INDEX.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
